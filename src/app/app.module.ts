18ffc0a39e5d236bb9a8560fae7b3199fae8d070
AdrianICH <adriancuerva2004@gmail.com>
2021-11-27 18:09:19 -0500

    Componente portafolios agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './components/app/app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { ContactanosComponent } from './components/contactanos/contactanos.component';
+import { PortafoliosComponent } from './components/portafolios/portafolios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactanosComponent,
+    PortafoliosComponent,
   ],
   imports: [
     BrowserModule,
